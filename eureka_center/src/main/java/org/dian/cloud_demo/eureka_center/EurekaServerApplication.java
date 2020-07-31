03f8057bdca93cb98101ee8f6e2919de751fc908
dian <956461675@qq.com>
2020-07-31 16:08:15 +0800

    初始提交

---
+++ eureka_center/src/main/java/org/dian/cloud_demo/eureka_center/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.dian.cloud_demo.eureka_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description: 启动类
+ * @Author: Liu Guo Dian
+ * @Date: 2020/7/31 15:14
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
